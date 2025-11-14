df4d1111e7004e70b797d5dda669e8b351586d5a
Ayan Khatib <ayankhatib0@gmail.com>
2025-11-14 22:38:06 +0530

    Cors issue solved

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 //routes
 
@@ -12,14 +13,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
+app.options("*", cors()); // ← Fix preflight
+
 // Middleware to parse JSON
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB blog database"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
